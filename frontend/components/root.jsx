87e11cfaad66dbd1c0c2ee9dd55dc407074910c0
patrickc <patrick.evan.campbell@gmail.com>
2022-01-27 14:33:01 -0800

    .

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./app";
 
-
 const Root = ({ store }) => (
     <Provider store={store}>
         <HashRouter>
@@ -11,6 +10,5 @@ const Root = ({ store }) => (
         </HashRouter>
     </Provider>
 );
-
 
 export default Root;
